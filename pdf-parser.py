fef501ff31def244538e0fbb4d2d06f54b0d3e06
Hannah S <hannahsartin@gmail.com>
2021-01-13 16:02:26 -0600

    Initial commit

---
+++ pdf-parser.py
@@ -0,0 +1,20 @@
+import fitz
+import os
+
+directory_in_str = "pdfs"
+directory = os.fsencode(directory_in_str)
+
+for file in os.listdir(directory):
+    pdfBt = os.fsencode(file)
+    pdf_document = "pdfs/" + pdfBt.decode("utf-8")
+    doc = fitz.open(pdf_document)
+
+    for current_page in range(len(doc)):
+        page = doc.loadPage(current_page)
+        pageText = page.getText("text")
+
+        file = open("pdf-txt.txt", "a+", encoding="utf8")
+        file.write(pageText)
+        file.close()
+
+# TODO: fix no objects found error
