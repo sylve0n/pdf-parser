9fae0e5fb2e02b3fe5e42c7f96e197107afb4bef
Hannah S <hannahsartin@gmail.com>
2021-01-14 14:45:20 -0600

    Added check for non-pdf documents

--- pdf-parser.py
+++ pdf-parser.py
@@ -1,5 +1,6 @@
 import fitz
 import os
+from pathlib import Path
 
 directory_in_str = "pdfs"
 directory = os.fsencode(directory_in_str)
@@ -7,16 +8,18 @@ directory = os.fsencode(directory_in_str)
 for file in os.listdir(directory):
     pdfBt = os.fsencode(file)
     pdf_document = "pdfs/" + pdfBt.decode("utf-8")
-    doc = fitz.open(pdf_document)
+    
+    if Path(pdf_document).suffix == ".pdf":
+        doc = fitz.open(pdf_document)
+        print(pdf_document)
 
     for current_page in range(len(doc)):
         page = doc.loadPage(current_page)
         pageText = page.getText("text")
-
+        # print(current_page)
         file = open("pdf-txt.txt", "a+", encoding="utf8")
         file.write(pageText)
         file.close()
 
-#TODO: 
-# iterate through docs to make sure they're actually pdfs
-# Remove unwanted characters, IE ‖, ‗, ―
+#TODO:
+# Remove unwanted characters, IE ‖ (U+2016), ‗ (U+2017), ― (U+2015), „ (U+201E)
